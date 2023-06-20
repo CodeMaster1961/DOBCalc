da47501633d03a4c7fd0910aa8297fecb32981e0
omer <omer.aynaci@hva.nl>
2023-06-20 16:09:11 +0200

    Toast added onclick

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.dobcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
+
+        buttonDatePicker.setOnClickListener {
+            Toast.makeText(this, "buttonDatePicker pressed", Toast.LENGTH_LONG).show()
+        }
     }
 }
