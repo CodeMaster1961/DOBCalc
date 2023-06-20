e8a607c94214d537e1911d216d93822d4dcfc5a0
omer <omer.aynaci@hva.nl>
2023-06-20 16:31:24 +0200

    function created for displaying date that is selected

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,24 @@ class MainActivity : AppCompatActivity() {
         val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
 
         buttonDatePicker.setOnClickListener {
-            Toast.makeText(this, "buttonDatePicker pressed", Toast.LENGTH_LONG).show()
+            clickDatePicker()
         }
+    }
+
+    fun clickDatePicker() {
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(
+            this, { view, year, month, dayOfMonth ->
+                Toast.makeText(
+                    this,
+                    "Year was $year, month was ${month + 1}, day of month was $dayOfMonth",
+                    Toast.LENGTH_LONG
+                ).show()
+            }, year, month, day
+        ).show()
+
     }
 }
